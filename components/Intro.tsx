4b0e014887b9351eb6512a6c04210838516a42ac
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2023-12-10 17:04:52 +0530

    Intro section completed

--- components/Intro.tsx
+++ components/Intro.tsx
@@ -6,8 +6,14 @@ import { BsArrowRight, BsLinkedin } from "react-icons/bs";
 import { HiDownload } from "react-icons/hi";
 import { FaGithubSquare } from "react-icons/fa";
 import Link from "next/link";
+import AvatarImg from "@/public/Noman_Avatar.jpeg";
 
 const Intro = () => {
+  const [useFallback, setUseFallback] = React.useState(false);
+
+  const handleImageError = () => {
+    setUseFallback(true);
+  };
   return (
     <section className="mb-28 max-w-[50rem] text-center sm:mb-0">
       <div className="flex items-center justify-center">
@@ -18,13 +24,19 @@ const Intro = () => {
             transition={{ type: "tween", duration: 0.2 }}
           >
             <Image
-              src={"https://avatars.githubusercontent.com/u/74728455?v=4"}
+              // src={"https://avatars.githubusercontent.com/u/74728455?v=4"}
+              src={
+                useFallback
+                  ? AvatarImg
+                  : "https://avatars.githubusercontent.com/u/74728455?v=4"
+              }
               alt="avatar_logo"
               width={"192"}
               height={"192"}
               quality={"95"}
               priority={true}
               className="h-24 w-24 rounded-full border-white border-[0.35rem] object-cover shadow-xl"
+              onError={handleImageError}
             />
           </motion.div>
           <motion.span
@@ -42,8 +54,8 @@ const Intro = () => {
           </motion.span>
         </div>
       </div>
-      <motion.h1
-        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-3xl"
+      <motion.p
+        className="mb-10 mt-4 px-4 text-xl font-medium !leading-[1.5] sm:text-2xl"
         initial={{ opacity: 0, y: 100 }}
         animate={{ opacity: 1, y: 0 }}
       >
@@ -52,7 +64,7 @@ const Intro = () => {
         <span className="font-bold">2 years</span> of experience. I enjoy
         building <span className="italic">sites & apps</span>. My focus is{" "}
         <span className="underline">MERN stack and (Next.js)</span>.
-      </motion.h1>
+      </motion.p>
       <motion.div
         className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
         initial={{ opacity: 0, y: 100 }}
@@ -66,19 +78,25 @@ const Intro = () => {
           Contact me here{" "}
           <BsArrowRight className="opacity-80 group-hover:translate-x-1 transition" />{" "}
         </Link>
-        <a className="group px-7 py-3 bg-white flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer">
+        <a
+          className="group px-7 py-3 bg-white flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-black/10"
+          href="/CV.pdf"
+          download
+        >
           Downlaod CV{" "}
           <HiDownload className="group-hover:translate-y-1 transition" />
         </a>
         <a
-          href=""
-          className="px-7 py-3 bg-white flex items-center gap-2 rounded-full text-gray-700 cursor-pointer"
+          href="https://www.linkedin.com/in/shaikh-mohd-noman-769917215/"
+          target="_blank"
+          className="p-4 bg-white flex items-center gap-2 rounded-full text-gray-700 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
         >
           <BsLinkedin />
         </a>
         <a
-          href=""
-          className="px-7 py-3 bg-white flex items-center gap-2 rounded-full text-gray-700 text-[1.35rem] cursor-pointer"
+          href="https://github.com/noman318"
+          target="_blank"
+          className="p-4 bg-white flex items-center gap-2 rounded-full text-gray-700 text-[1.35rem] cursor-pointer focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border-black/10"
         >
           <FaGithubSquare />
         </a>
