e28f17a43a5b890f5d6dfe488eeddc8b7c497f25
Shaikh Noman <shaikhmohdnoman612@gmail.com>
2025-05-28 23:29:44 +0530

    dark mode added in app.

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -27,15 +27,16 @@ const Contact = () => {
       }}
     >
       <SectionHeading>Contact Me</SectionHeading>
-      <p className=" text-gray-700">
+      <p className="text-gray-700 -mt-6 dark:text-white/80">
         Please contact me directly at{" "}
         <a className="underline" href="mailto:shaikhmohdnoman612@gmail.com">
           shaikhmohdnoman612@gmail.com
         </a>{" "}
         or through this form.
       </p>
+
       <form
-        className="mt-10 flex flex-col"
+        className="mt-10 flex flex-col dark:text-black"
         action={async (formData) => {
           const { data, error } = await sendEmail(formData);
 
@@ -48,7 +49,7 @@ const Contact = () => {
         }}
       >
         <input
-          className="h-14 px-4 rounded-lg borderBlack transition-all dark:outline-none"
+          className="h-14 px-4 rounded-lg borderBlack dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="senderEmail"
           type="email"
           required
@@ -56,7 +57,7 @@ const Contact = () => {
           placeholder="Your email"
         />
         <textarea
-          className="h-52 my-3 rounded-lg borderBlack p-4 transition-all dark:outline-none"
+          className="h-52 my-3 rounded-lg borderBlack p-4 dark:bg-white dark:bg-opacity-80 dark:focus:bg-opacity-100 transition-all dark:outline-none"
           name="message"
           placeholder="Your message"
           required
--- components/Intro.tsx
+++ components/Intro.tsx
@@ -8,10 +8,12 @@ import { FaGithubSquare } from "react-icons/fa";
 import Link from "next/link";
 import AvatarImg from "@/public/Noman_Avatar.jpeg";
 import { useSectionInView } from "@/hooks/hooks";
+import { useActiveSectionContext } from "@/context/ActiveSectionContext";
 
 const Intro = () => {
   const { ref } = useSectionInView("Home", 0.5);
   const [useFallback, setUseFallback] = React.useState(false);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
 
   const handleImageError = () => {
     setUseFallback(true);
@@ -78,31 +80,38 @@ const Intro = () => {
         transition={{ delay: 0.1 }}
       >
         <Link
-          href={"#contact"}
-          className="group px-7 py-3 bg-gray-900 text-white flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          href="#contact"
+          className="group bg-gray-900 text-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 hover:bg-gray-950 active:scale-105 transition"
+          onClick={() => {
+            setActiveSection("Contact");
+            setTimeOfLastClick(Date.now());
+          }}
         >
           Contact me here{" "}
-          <BsArrowRight className="opacity-80 group-hover:translate-x-1 transition" />{" "}
+          <BsArrowRight className="opacity-70 group-hover:translate-x-1 transition" />
         </Link>
+
         <a
-          className="group px-7 py-3 bg-white flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer border-black/10"
-          href="/frontend_dev_noman_cv.pdf"
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/CV.pdf"
           download
         >
-          Downlaod CV{" "}
-          <HiDownload className="group-hover:translate-y-1 transition" />
+          Download CV{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
         </a>
+
         <a
-          href="https://www.linkedin.com/in/shaikh-mohd-noman-769917215/"
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://linkedin.com"
           target="_blank"
-          className="p-4 bg-white flex items-center gap-2 rounded-full text-gray-700 focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer border-black/10"
         >
           <BsLinkedin />
         </a>
+
         <a
-          href="https://github.com/noman318"
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com"
           target="_blank"
-          className="p-4 bg-white flex items-center gap-2 rounded-full text-gray-700 text-[1.35rem] cursor-pointer focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition border-black/10"
         >
           <FaGithubSquare />
         </a>
